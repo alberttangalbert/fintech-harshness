a8c0292877cb69a205508a9fc51eed4283bcdcbf
alberttangalbert <66285052+alberttangalbert@users.noreply.github.com>
2023-10-04 22:26:53 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+#source: https://medium.com/@sharma.tanish096/sentiment-analysis-using-pre-trained-models-and-transformer-28e9b9486641
+import numpy as np
+from utils import get_all_data, get_wells_fargo_data
+import re
+import time
+import random
+#%%
+wells_fargo_data = get_wells_fargo_data()
+
+#%%
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+complaints = list(wells_fargo_data["Consumer complaint narrative"].dropna())
+random.seed(0)
+random.shuffle(complaints)
+complaint_index = 0
+
+@app.route("/")
+def index():
+    return render_template("index.html", complaints=complaints)
+
+if __name__ == "__main__":
+    app.run()
